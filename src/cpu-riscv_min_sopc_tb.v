1f1ef3d97287227b3da4611c782ad9aef5219531
inrence@outlook.com <root@DESKTOP-J2ND151.localdomain>
2017-12-18 21:29:02 +0800

    finished chapter4

---
+++ src/cpu-riscv_min_sopc_tb.v
@@ -0,0 +1,26 @@
+'timescale 1ns / 1ps
+
+module cpu_riscv_min_sopc_tb (
+
+        reg CLOCK_50;
+        reg rst;
+    );
+
+    // 每10ns反转一次clk信号 一周期20ns 频率 50MHz
+    initial begin
+        CLOCK_50 = 1'b0;
+        forever #10 CLOCK_50 = ~CLOCK_50;
+    end
+
+    initial begin
+        rst = `RstEnable;
+        #195 rst = `RstDisable;
+        #1000 $stop;
+    end
+
+    cpu_riscv_min_sopc cpu_riscv_min_sopc0(
+            .clk(CLOCK_50);
+            .rst(rst);
+        );
+
+endmodule // cpu_riscv_min_sopc_tb
--- src/inst_rom.v
+++ src/inst_rom.v
@@ -10,14 +10,14 @@ module inst_rom(
 
     reg[`InstBus]  inst_mem[0:`InstMemNum-1]; // 以指令为单位
 
-    // initial $readmemh ("inst_rom.data", inst_mem);
+    initial $readmemh ("inst_rom.data", inst_mem);
 
     always @ (*) begin
         if (ce == `ChipDisable) begin
             inst <= `ZeroWord;
         end else begin
-            inst <= inst_mem[addr[`InstMemNumLog2+1:2]];
-            // 地址是以byte为单位 除以4再使用
+            inst <= {inst_mem[addr >> 2][7:0], inst_mem[addr >> 2][15:8],
+                     inst_mem[addr >> 2][23:16], inst_mem[addr >> 2][31:24]};
         end
     end
 
